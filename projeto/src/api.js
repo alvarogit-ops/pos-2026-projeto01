b90493e314409d065cd97d62b301f5706298f7c4
Álvaro Yuri <alvaro.yuri@outlook.com>
2026-08-12 14:29:46 -0300

    roda o projeto com vite

---
+++ projeto/src/api.js
@@ -0,0 +1,7 @@
+const api = "https://dummyjson.com/"
+
+export async function buscarProdutos(){
+    try{
+        const response = await fetch (`${api}/`)
+    }
+}
---
+++ projeto/src/main.js
@@ -0,0 +1,11 @@
+async function = carregarProdutos(){
+    const response = await fetch('https://dummyjson.com/products')
+    const data = await response.json()
+
+    console.log(data)
+}
+
+carregarProdutos()
+/*fetch('https://dummyjson.com/products')
+  .then(res => res.json())
+  .then(data => console.log(data)) */
